3dcc59a9e277daddcde143ed6a1940529c38acb5
hailaicon <169118104+hailaicon@users.noreply.github.com>
2024-07-01 11:36:52 +0700

    menu

---
+++ Assets/script/ChangeScene.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject panel;
+    public float load = 0;
+    public Slider loadslider;
+    public Text loadtext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(load >= 100)
+        {
+            SceneManager.LoadScene("Level0");
+        }
+    }
+    public void play()
+    {
+        panel.SetActive(true);
+        StartCoroutine(loadingbar());
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    IEnumerator loadingbar()
+    {
+        while(load < 100)
+        {
+            load += 1f;
+            loadslider.value = load;
+            loadtext.text = "Loading " + load + "%";
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}

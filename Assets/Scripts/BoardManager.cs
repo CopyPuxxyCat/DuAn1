c746aecffdaf1b35f45d4d95937ab31dfd48986c
CopyPuxxyCat <johnnynguyenkuli218@gmail.com>
2024-07-19 17:36:32 +0700

    character and slime
    Hung_5:36_19/07/2024

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -447,7 +447,7 @@ public class BoardManager : MonoBehaviour {
                 int roomHeight = (int)subdungeon.room.height;
 
                 // Calculate the number of monsters based on room size
-                int numberOfMonsters = Mathf.Max(1, 2);
+                int numberOfMonsters = Mathf.Max(1, 3);
 
                 Vector3 roomCenter = new Vector3(
                     subdungeon.room.x + subdungeon.room.width / 2,
---
+++ Assets/Scripts/Players/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamagable
+{
+    public float Health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public bool targetAble { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public bool invincible { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public float _health = 3f;
+    public bool _targetable = true;
+
+    public void OnHit(float damage, Vector2 knockback)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHit(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnObjectDestroyed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}

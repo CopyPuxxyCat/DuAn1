abcd047b3929c303503bbffccafece3fbe6baa87
jacob27012005 <169118214+jacob27012005@users.noreply.github.com>
2024-07-30 22:31:30 +0700

    update gameobver

--- Assets/Scripts/Players/DamageableCharacter.cs
+++ Assets/Scripts/Players/DamageableCharacter.cs
@@ -23,6 +23,8 @@ public class DamageableCharacter : MonoBehaviour, IDamagable
     Rigidbody2D rb;
 
     bool isAlive = true;
+
+    public GameObject GameOver;
     public float Health
     {
         set
@@ -48,6 +50,8 @@ public class DamageableCharacter : MonoBehaviour, IDamagable
                 // tat slime
                 targetAble = false;
                 // hien panel gameover
+                Time.timeScale = 0f;
+                GameOver.SetActive(true);
             }
         }
         get

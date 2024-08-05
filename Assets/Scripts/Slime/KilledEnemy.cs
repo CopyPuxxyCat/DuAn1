332c5c47b6cb9e5b8793bef85ff14eabb627a265
CopyPuxxyCat <johnnynguyenkuli218@gmail.com>
2024-08-05 12:57:21 +0700

    CC

--- Assets/Scripts/Slime/KilledEnemy.cs
+++ Assets/Scripts/Slime/KilledEnemy.cs
@@ -1,9 +1,11 @@
 
 
 using System.Diagnostics;
+using System.Numerics;
+using UnityEngine;
 
 public static class KilledEnemy
 {
     public static int sharedValue;
-    
+    public static UnityEngine.Vector3 enemyVector3;
 }

63179f3bb09574d84c2f03e645a9b9d53af70de2
hailaicon <169118104+hailaicon@users.noreply.github.com>
2024-08-11 15:58:56 +0700

    Revert "Merge pull request #149 from CopyPuxxyCat/T.Huy"
    This reverts commit 0284fc3ab296868dedfaab68a4367427e44d686a, reversing
    changes made to 7d610f0fb341844d2b00e5a5933bfb792c1755f7.

--- Assets/Scripts/Players/DamageableCharacter.cs
+++ Assets/Scripts/Players/DamageableCharacter.cs
@@ -123,9 +123,8 @@ public class DamageableCharacter : MonoBehaviour, IDamagable
         {
             if (gameObject.CompareTag("Player"))
             {
-                
+                KilledEnemy.player_health_Manager = player_health;
                 return player_health;
-
             }
             else if (gameObject.CompareTag("Enemy"))
             {
